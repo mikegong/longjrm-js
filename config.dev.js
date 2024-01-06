be478700bcdb427022d1a4ed02da2ad709eb984e
walkergg <uniwalker@139.com>
2024-01-06 13:43:49 -0500

    add env config examples

--- config.dev.js
+++ config.dev.js
@@ -1,7 +1,4 @@
 const config = {
-  'API_PORT': 8000,
-  'HTTPS_PORT': 8001,
-  'WEB_PORT': 3000,
   'DB_TIMEOUT': 40, // database connection timeout
   'DATA_FETCH_LIMIT': 1000, // default data fetch limit. 0 means unlimit
   'MIN_CONN_POOL_SIZE': 2,
