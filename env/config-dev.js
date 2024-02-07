2e0acbb8b172d2ca95fffe3f2bbd25bc4a88ac65
walkergg <uniwalker@139.com>
2024-02-07 13:20:53 -0500

    improve pino log with pretty format, etc.

---
+++ env/config-dev.js
@@ -0,0 +1,10 @@
+const config = {
+  'DB_TIMEOUT': 40, // database connection timeout
+  'DATA_FETCH_LIMIT': 1000, // default data fetch limit. 0 means unlimit
+  'MIN_CONN_POOL_SIZE': 2,
+  'MAX_CONN_POOL_SIZE': 50,
+  'ACQUIRE_TIMEOUT': 10000,
+  'DESTROY_TIMEOUT': 5000,
+}
+
+export default config;
---
+++ env/dbinfos-dev-example.js
@@ -0,0 +1,28 @@
+const dbInfos =
+{
+    "mysql-test": {
+        "type": "mysql",
+        "host": "localhost",
+        "user": "",
+        "password": "",
+        "port": 3306,
+        "database": "test"
+    },
+    "postgres-test": {
+        "type": "postgres",
+        "host": "localhost",
+        "user": "",
+        "password": "",
+        "port": 5432,
+        "database": "test"
+    },
+    "mongodb-test": {
+        "type": "mongodb+srv",
+        "host": "",
+        "user": "",
+        "password": "",
+        "database": "test"
+    }
+}
+
+export default dbInfos;
